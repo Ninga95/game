d0514b0087de7d9d3fd54f17601209921e589925
Jake Nanninga <jninga95@gmail.com>
2015-06-11 15:05:26 +1000

    gam111_2

---
+++ Assets/scripts/EnemyController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	public Transform visionPoint;
+	private PlayerController player;
+
+	public float visionAngle = 30f;
+	public float visionDistance = 10f;
+	public float moveSpeed = 2f;
+	public float chaseDistance =3f;
+
+	private Vector3? lastKnownPlayerPosition;
+	// Use this for initialization
+
+	void Start () {
+		player = GameObject.FindObjectOfType<PlayerController> ();
+	}
+
+	void Look()
+	{
+		Vector3 deltaToPlayer = player.transform.position - visionPoint.position;
+		Vector3 directionToPlayer = deltaToPlayer.normalized;
+		float distanceToPlayer = directionToPlayer.magnitude;
+
+		float dot = Vector3.Dot (transform.forward, directionToPlayer); 
+
+		if (dot < 0) 
+		{
+				return;
+		}
+
+		if (distanceToPlayer > visionDistance) 
+		{
+				return;
+			}
+
+		float angle = Vector3.Angle (transform.forward, directionToPlayer);
+
+		if (angle > visionAngle)
+		{
+			return;
+		}
+
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, directionToPlayer, out hit, visionDistance))
+		{
+			if (hit.collider.gameObject == player.gameObject)
+			{
+				lastKnownPlayerPosition = player.transform.position;
+			}
+	}
+	}
+	
+	void Act()
+		{
+		}
+	// Update is called once per frame
+	void Update() 
+		{
+		}
+
+	void FixedUpdate() 
+		{
+		}
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour {
+
+	public float inputTurn;
+	public float inputForward;
+	public float inputSide;
+
+	public float turnSpeed = 300;
+	public float moveSpeed = 5;
+
+	CharacterController characterController;
+
+	void Awake() {
+		characterController = GetComponent<CharacterController> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		inputTurn = Input.GetAxis ("Mouse X");
+
+		inputForward = Input.GetAxis ("Vertical");
+		inputSide = Input.GetAxis ("Horizontal");
+	}
+
+	void FixedUpdate ()
+	{
+		transform.Rotate (Vector3.up, inputTurn * turnSpeed * Time.fixedDeltaTime);
+
+		Vector3 delta = Vector3.zero;
+
+		delta += transform.forward * -inputForward * moveSpeed * Time.fixedDeltaTime;
+		delta += transform.right * -inputSide * moveSpeed * Time.fixedDeltaTime;
+
+		characterController.Move (delta); 
+	}
+
+}
